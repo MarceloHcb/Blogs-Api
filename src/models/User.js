34917f8354611ec0b4ff11f800a06dc2d6bcb91b
MarceloHcb <marcelohcb@msn.com>
2023-05-10 16:50:54 -0300

    cria a model User, req2

---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      display_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      underscore: true,
+      timestamps: false,
+      tableName: "users",
+    }
+  );
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreingkey: "user_id",
+      as: "users",
+    });
+  };
+  return User;
+};
